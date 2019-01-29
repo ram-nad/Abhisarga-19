cf927afe090f0fcbc96438571f899ed37563d6ac
Ram Nad <44163356+ram-nad@users.noreply.github.com>
2019-01-29 09:03:08 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,25 @@
+function update_time(){
+    var calcNewYear = setInterval(function(){
+    date_now = new Date();
+    date_future = new Date(date_now.getFullYear(), 2, 22);
+
+    seconds = Math.floor((date_future - (date_now))/1000);
+    minutes = Math.floor(seconds/60);
+    hours = Math.floor(minutes/60);
+    days = Math.floor(hours/24);
+
+    hours = hours-(days*24);
+    minutes = minutes-(days*24*60)-(hours*60);
+    seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+
+    day_section = document.getElementById('timer_day');
+    hour_section = document.getElementById('timer_hour');
+    minute_section = document.getElementById('timer_minute');
+    second_section = document.getElementById('timer_second');
+
+    day_section.innerHTML = days;
+    hour_section.innerHTML = hours;
+    minute_section.innerHTML = minutes;
+    second_section.innerHTML = seconds;
+  },1000);
+}
